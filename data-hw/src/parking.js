37f15df4fc99a5d25008405f30005f97658f0d5a
Carlyse Jordan <carlyse.jordan@Carlyses-MacBook-Air.local>
2017-03-06 00:24:29 -0500

    starting to set up my function and produce a result

--- data-hw/src/parking.js
+++ data-hw/src/parking.js
@@ -1,8 +1,30 @@
-let read = require('./parse.js');
+  let read = require('./parse.js');
 
-let parkingData = read('./traffic-data/simple_data/parking_feb_2016.csv');
+  function parkingData() {
 
-module.exports = function parking(year, monthName){
-  
+    let parking = read('./traffic-data/simple_data/moving_jan_2016.csv');
 
-};
+    //most common violation type for parking
+
+    let frequency = {};
+     // no input in the object bracket to find answer for frequency
+     //
+
+     //making a loop ".forEach"
+     parking.foreach( function propertyItem(row) {
+
+       //call/run/execute the function
+       //most common violation type for parking
+       //should log out row number
+       if(!frequency[row[9]]) {
+         //if statements take args of what needs to happen
+         //otherwise "else" do this; should give the index 9 of the row if frequency
+         //falsey
+          frequency[row[9]] = 1;
+        } else {
+          //if freq defined add 1
+        }frequency[row[9]] = frequency[row[9]]++;
+       //this should give back a violation type and count
+     }
+
+});
